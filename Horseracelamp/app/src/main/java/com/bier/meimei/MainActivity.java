6daa61986fc64adfcae41c36c7cfeb8ada7d6d31
nameVal <1240604448@qq.com>
2019-06-04 09:41:05 +0800

    第一次提交马甲2

--- Horseracelamp/app/src/main/java/com/bier/meimei/MainActivity.java
+++ Horseracelamp/app/src/main/java/com/bier/meimei/MainActivity.java
@@ -2,6 +2,7 @@ package com.bier.meimei;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);    // 去掉标题
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
--- Horseracelamp/app/src/main/java/com/bier/meimei/view/MarqueeTextView.java
+++
@@ -1,6 +0,0 @@
-package com.bier.meimei.view;
-
-import android.widget.TextView;
-
-public class MarqueeTextView extends TextView {
-}
